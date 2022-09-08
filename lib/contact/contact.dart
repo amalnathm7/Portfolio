42657b2bc075c6e423a03a14b7f382b47176be74
Amal Nath M <amalnathm7@gmail.com>
2022-09-08 15:31:40 +0530

    Contact initial done

--- lib/contact/contact.dart
+++ lib/contact/contact.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:portfolio/main.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactPage extends StatefulWidget {
   const ContactPage({Key? key}) : super(key: key);
@@ -15,85 +16,255 @@ class _ContactPageState extends State<ContactPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Stack(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Center(
-          child: Column(
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Lottie.network(
-                      "https://assets10.lottiefiles.com/packages/lf20_wbhpdrhp.json",
-                      height: size.width * 0.15,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: size.width * 0.08,
+                      bottom: size.width * 0.01,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "CONT",
-                          style: GoogleFonts.kanit(
-                            fontSize: size.width * 0.05,
-                            fontWeight: FontWeight.bold,
-                            color: Colours.primary,
-                          ),
-                        ),
-                        Text(
-                          "ACT",
-                          style: GoogleFonts.kanit(
-                            fontSize: size.width * 0.05,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[800],
-                          ),
-                        ),
-                      ],
+                    child: Text(
+                      "I would love to discuss more opportunities. So shoot away!",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.caveat(
+                        fontSize: size.width * 0.014,
+                      ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "M",
-                          style: GoogleFonts.kanit(
-                            fontSize: size.width * 0.05,
-                            fontWeight: FontWeight.bold,
-                            color: Colours.primary,
-                          ),
-                        ),
-                        Text(
-                          "E.",
-                          style: GoogleFonts.kanit(
-                            fontSize: size.width * 0.05,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[800],
-                          ),
-                        ),
-                      ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: size.width * 0.01,
+                      bottom: size.width * 0.01,
                     ),
-                  ],
-                ),
+                    child: SizedBox(
+                      width: size.width * 0.2,
+                      child: TextField(
+                        cursorColor: Colours.primary,
+                        decoration: const InputDecoration(
+                          hintText: "Name",
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: size.width * 0.01,
+                      bottom: size.width * 0.01,
+                    ),
+                    child: SizedBox(
+                      width: size.width * 0.2,
+                      child: TextField(
+                        cursorColor: Colours.primary,
+                        decoration: const InputDecoration(
+                          hintText: "Email",
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: size.width * 0.01,
+                      bottom: size.width * 0.01,
+                    ),
+                    child: SizedBox(
+                      width: size.width * 0.2,
+                      child: TextField(
+                        cursorColor: Colours.primary,
+                        decoration: const InputDecoration(
+                          hintText: "Project Domain",
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: size.width * 0.01,
+                      bottom: size.width * 0.01,
+                    ),
+                    child: SizedBox(
+                      width: size.width * 0.2,
+                      child: TextField(
+                        minLines: 5,
+                        maxLines: 10,
+                        cursorColor: Colours.primary,
+                        decoration: const InputDecoration(
+                          hintText: "Project Description",
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: size.width * 0.01,
+                      bottom: size.width * 0.01,
+                    ),
+                    child: MaterialButton(
+                      color: Colours.primary,
+                      onPressed: () {},
+                      child: SizedBox(
+                        height: size.width * 0.018,
+                        width: size.width * 0.07,
+                        child: Center(
+                          child: Text(
+                            "JUST SEND",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.kanit(
+                              fontSize: size.width * 0.008,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
-              Container(
-                height: size.width * 0.05,
-                color: Colours.primary,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Lottie.network(
+                          "https://assets10.lottiefiles.com/packages/lf20_wbhpdrhp.json",
+                          height: size.width * 0.15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "CONT",
+                              style: GoogleFonts.kanit(
+                                fontSize: size.width * 0.05,
+                                fontWeight: FontWeight.bold,
+                                color: Colours.primary,
+                              ),
+                            ),
+                            Text(
+                              "ACT",
+                              style: GoogleFonts.kanit(
+                                fontSize: size.width * 0.05,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[800],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "M",
+                              style: GoogleFonts.kanit(
+                                fontSize: size.width * 0.05,
+                                fontWeight: FontWeight.bold,
+                                color: Colours.primary,
+                              ),
+                            ),
+                            Text(
+                              "E.",
+                              style: GoogleFonts.kanit(
+                                fontSize: size.width * 0.05,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[800],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: EdgeInsets.all(size.width * 0.02),
-            child: Text(
-              "Made with Flutter ✨",
-              style: TextStyle(
-                fontSize: size.width * 0.008,
-                color: Colors.grey[800],
+        Container(
+          height: size.width * 0.05,
+          width: size.width,
+          color: Colours.primary,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () async {
+                        if (!await launchUrl(Uri.parse(
+                            "mailto:amalnathm7@gmail.com?subject=Hey Amal, I have something exciting for you!"))) {
+                          throw 'Could not launch';
+                        }
+                      },
+                      child: Text(
+                        "amalnathm7@gmail.com",
+                        style: TextStyle(
+                          fontSize: size.width * 0.008,
+                          color: Colors.grey[900],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.05,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "©️2022 Amal Nath M",
+                          style: TextStyle(
+                            fontSize: size.width * 0.007,
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Made with Flutter",
+                            style: TextStyle(
+                              fontSize: size.width * 0.005,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: size.width * 0.05,
+                    ),
+                    Text(
+                      "(+91) 790 758 7380",
+                      style: TextStyle(
+                        fontSize: size.width * 0.008,
+                        color: Colors.grey[900],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ],
